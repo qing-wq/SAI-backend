bda4c0b8908a93e70cd4c20716a7d2ffbfc9c267
qing-wq <2702461713@qq.com>
2022-11-15 20:49:11 +0800

    update

--- src/main/java/ink/whi/saibackend/controller/DataController.java
+++ src/main/java/ink/whi/saibackend/controller/DataController.java
@@ -9,6 +9,7 @@ import jdk.nashorn.api.scripting.JSObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class DataController {
     @Autowired
     StuService service;
 
+    @CrossOrigin
     @RequestMapping("/submit")
     public String submit(@RequestBody String json) throws IOException {
         try {
