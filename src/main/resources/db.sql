532a05c4a6e4310b5950f279c6b9b04ee2b99b96
qing-wq <2702461713@qq.com>
2022-11-15 20:35:18 +0800

    update

---
+++ src/main/resources/db.sql
@@ -0,0 +1,26 @@
+create table ainfo
+(
+    sid       int                       not null
+        primary key,
+    level     int                       not null,
+    direction varchar(255)              not null,
+    other     varchar(255) default '无' null
+);
+
+create table language
+(
+    type varchar(255) not null,
+    sid  int(10)      not null,
+    primary key (type, sid)
+);
+
+create table stuinfo
+(
+    name       varchar(255) not null comment 'name',
+    id         int          not null
+        primary key,
+    email      varchar(255) not null,
+    qq         varchar(255) not null,
+    management varchar(1)   not null,
+    introduce  varchar(900) not null
+);
