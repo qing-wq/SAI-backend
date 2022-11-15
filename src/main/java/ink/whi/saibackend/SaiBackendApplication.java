1697c34e911804e2cd10740f8a276f2baffe461e
qing-wq <2702461713@qq.com>
2022-11-15 11:15:40 +0800

    init

---
+++ src/main/java/ink/whi/saibackend/SaiBackendApplication.java
@@ -0,0 +1,13 @@
+package ink.whi.saibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ink/whi/saibackend/SaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.whi.saibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
