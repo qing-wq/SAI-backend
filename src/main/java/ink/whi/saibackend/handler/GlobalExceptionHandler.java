9263536bf8420eb46160d8ced7acea776ab2421f
qing-wq <2702461713@qq.com>
2022-11-19 18:35:45 +0800

    update

--- src/main/java/ink/whi/saibackend/handler/GlobalExceptionHandler.java
+++ src/main/java/ink/whi/saibackend/handler/GlobalExceptionHandler.java
@@ -1,17 +1,17 @@
-//package ink.whi.saibackend.handler;
-//
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//@ControllerAdvice
-//@ResponseBody
-//public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(value = Exception.class)
-//    public String unauthorizedHandler(HttpServletRequest request, Exception exception) throws Exception {
-//        return "Server exception";
-//    }
-//}
+package ink.whi.saibackend.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public String unauthorizedHandler(HttpServletRequest request, Exception exception) throws Exception {
+        return "Server exception";
+    }
+}
